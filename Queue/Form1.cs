3afe21f44359c52501c568eb4861302f33f5c104
Eugene Frolov <spanditime@gmail.com>
2022-09-23 12:48:32 +0300

    added solution for queue task

---
+++ Queue/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SAOD;
+
+namespace Queue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Queue<string> queue;
+
+        private void push_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                queue.Push(input.Text);
+                input.Text = "";
+            }
+            catch (Exception exc)
+            {
+                push.Enabled = false;
+            }
+            updateText();
+        }
+
+        private void pop_Click(object sender, EventArgs e)
+        {
+            push.Enabled = true;
+            if (!queue.IsEmpty)
+            {
+                queue.Pop();
+            }
+            updateText();
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            input.Enabled = true;
+            queue.Clear();
+            updateText();
+        }
+
+        private void updateText()
+        {
+            string Text = "";
+            string[] arr = queue.Values;
+            for (int i = 0; i < queue.Size; i++)
+            {
+                Text += arr[i];
+                if (i != queue.Size - 1)
+                    Text += ",";
+            }
+            output.Text = Text;
+        }
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAOD
+{
+    public class Queue<T>
+    {
+        public uint Capacity { get; private set; }
+        public uint Size { get; private set; }
+        public uint First { get; private set; }
+        public T[] Values { get; private set; }
+        public Queue(uint size)
+        {
+            this.Capacity = size;
+            this.Size = 0;
+            First = 0;
+            this.Values = new T[this.Capacity];
+        }
+        public void Push(T value)
+        {
+            if (Size == Capacity)
+                throw new Exception();
+            Values[(First + Size) % Capacity] = value;
+        }
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new Exception();
+            return Values[First];
+        }
+        public T Pop()
+        {
+            T value = Peek();
+            Size--;
+            First = (First + 1) % Capacity;
+            return value;
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return Size == 0;
+            }
+        }
+        public void Clear()
+        {
+            Size = 0;
+        }
+    }
+}
