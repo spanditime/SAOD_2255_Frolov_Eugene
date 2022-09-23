7a3c7b996976d7bb0cc546e35f66f91f8df89488
Eugene Frolov <spanditime@gmail.com>
2022-09-23 12:45:01 +0300

    added solution for stack task

---
+++ Stack/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SAOD;
+
+namespace Stack
+{
+    public partial class Form1 : Form
+    {
+        private Stack<string> stack;
+        public Form1()
+        {
+            InitializeComponent();
+            stack = new Stack<string>(20);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void push_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                stack.Push(input.Text);
+                input.Text = "";
+            } catch( Exception exc)
+            {
+                push.Enabled = false;
+            }
+            updateText();
+        }
+
+        private void pop_Click(object sender, EventArgs e)
+        {
+            push.Enabled = true;
+            if (!stack.IsEmpty)
+            {
+                stack.Pop();
+            }
+            updateText();
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            input.Enabled = true;
+            stack.Clear();
+            updateText();
+        }
+
+        private void updateText()
+        {
+            string Text = "";
+            string[] arr = stack.Values;
+            for(int i =0; i < stack.Size; i++)
+            {
+                Text += arr[i];
+                if (i != stack.Size - 1)
+                    Text += ",";
+            }
+            output.Text = Text;
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAOD
+{
+    internal class Stack<T>
+    {
+        public uint Capacity { get; private set; }
+        public uint Size { get; private set; }
+        private T[] values;
+        public Stack(uint size)
+        {
+            if (size == 0)
+                throw new Exception();
+            this.Size = 0;
+            this.Capacity = size;
+            this.values = new T[this.Capacity];
+        }
+        public void Push(T value)
+        {
+            if (Size == Capacity)
+                throw new Exception();
+            values[Size] = value;
+            Size++;
+        }
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new Exception();
+            return values[Size - 1];
+        }
+        public T Pop()
+        {
+            T value = Peek();
+            Size--;
+            return value;
+        }
+
+        
+        public bool IsEmpty
+        {
+            get
+            {
+                return Size == 0;
+            }
+        }
+        public void Clear()
+        {
+            Size = 0;
+        }
+        public T[] Values
+        {
+            get
+            {
+                T[] ret = new T[Size];
+                for(int i = 0; i < Size; i++){
+                    ret[i] = values[i];
+                }
+                return ret;
+            }
+        }
+    }
+}
