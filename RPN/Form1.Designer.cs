cbe84d297415bcaa14e3ed631576e9156154dcd1
Eugene Frolov <spanditime@gmail.com>
2022-10-06 21:59:45 +0300

    added a solutiom for reverse polish notation task

---
+++ RPN/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace RPN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(522, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(385, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(424, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(111, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 80);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RPN/RPN.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAOD
+{
+    internal class RPN
+    {
+        List<string> rtokens = new List<string>();
+        List<string> tokens = new List<string>();
+        Dictionary<string, (uint, Func<double, double, double>)> operators = new Dictionary<string, (uint, Func<double, double, double>)> {
+            { "+", (1u,(l,r) => l+r) },
+            { "-", (1u,(l,r) => l-r) },
+            { "*", (2u,(l,r) => l*r) },
+            { "/", (2u,(l,r) => l/r) },
+            { "^", (3u,(l,r) => Math.Pow(l,r)) }
+        };
+        char[] separators = new char[] { '+', '-', '*', '\\','^', '(', ')' };
+        public RPN(string inp)
+        {
+            while (inp.Length > 0)
+            {
+                string curr = inp.Substring(0, 1);
+                inp = inp.Substring(1);
+                if (IsBracket(curr))
+                {
+                    tokens.Append(curr);
+                } else if (IsOperator(curr))
+                {
+                    if (IsOperator(tokens.Last()))
+                    {
+                        throw new Exception();
+                    }
+                    else if (tokens.Last() == "(")
+                    {
+
+                        // watch for number
+                        tokens.Append(GetNumber(ref inp, curr));
+                    }
+                    else
+                    {
+                        tokens.Append(curr);
+                    }
+                }
+                else
+                {
+                    //watch for number
+                    tokens.Append(GetNumber(ref inp, curr));
+                }
+            }
+
+
+            // form postfix notation
+            Stack<string> stack = new Stack<string>(tokens.Size);
+            List<string> list = tokens;
+            while (!list.IsEmpty)
+            {
+                string curr = list.First();
+                list.RemoveFront();
+                if (IsNumber(curr))
+                {
+                    rtokens.Append(curr);
+                }
+                else if (curr == "(")
+                {
+                    stack.Push(curr);
+                } else if (curr == ")")
+                {
+                    while (stack.Peek() != "(")
+                    {
+                        rtokens.Append(stack.Pop());
+                    }
+                    if (stack.IsEmpty)
+                        throw new Exception();
+                    stack.Pop();
+                }
+                else if (IsOperator(curr))
+                {
+                    while (true)
+                    {
+                        if (stack.IsEmpty)
+                            break;
+                        if (stack.Peek() == "(")
+                            break;
+                        if (!operators.ContainsKey(stack.Peek()))
+                            throw new Exception();
+                        if (operators[stack.Peek()].Item1 >= operators[curr].Item1)
+                        {
+                            rtokens.Append(stack.Pop());
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    stack.Push(curr);
+                }
+            }
+            while (!stack.IsEmpty)
+            {
+                if (!IsOperator(stack.Peek()))
+                    throw new Exception();
+                rtokens.Append(stack.Pop());
+            }
+        }
+
+        public double Result
+        {
+            get {
+                Stack<double> stack = new Stack<double>(rtokens.Size);
+                List<string> list = rtokens;
+
+                while (!list.IsEmpty)
+                {
+                    string curr = list.First();
+                    list.RemoveFront();
+                    if (IsOperator(curr))
+                    {
+                        double second = stack.Pop();
+                        double first = stack.Pop();
+                        stack.Push(operators[curr].Item2.Invoke(first,second));
+                    }
+                    else
+                    {
+                        stack.Push(Convert.ToDouble(curr));
+                    }
+                }
+                return stack.Peek();
+            }
+        } 
+        public string InfixNotation { 
+            get
+            {
+                string res = "";
+                for(uint i = 0; i < tokens.Size; i++)
+                {
+                    res += tokens.At(i);
+                }
+                return res;
+            } 
+        }
+        public string PostfixNotation
+        {
+            get
+            {
+                string res = "";
+                for(uint i = 0; i < rtokens.Size; i++)
+                {
+                    res += rtokens.At(i) + ((i == rtokens.Size - 1) ? ("") : (" "));
+                }
+                return res;
+            }
+        }
+        private string GetNumber(ref string inp, string curr)
+        {
+            int idx = inp.IndexOfAny(separators);
+            if (idx == -1)
+                idx = inp.Length;
+            string res = "";
+            if (idx == inp.Length)
+            {
+                res = curr + inp;
+                inp = "";
+            }
+            else
+            {
+                res = curr + inp.Substring(0, idx);
+                inp = inp.Substring(idx);
+            }
+
+            if (!IsNumber(res))
+                throw new Exception();
+            return res;
+        }
+        private bool IsNumber(string val)
+        {
+            try
+            {
+                Convert.ToDouble(val);
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+        }
+        private bool IsBracket(string val)
+        {
+            return val == "(" || val == ")";
+        }
+        private bool IsOperator(string val)
+        {
+            return operators.ContainsKey(val);
+        }
+    }
+}
